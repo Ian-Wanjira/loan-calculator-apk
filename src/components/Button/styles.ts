695bdcf72c82d69083daaed80fb4b06957a59f13
Ian-Wanjira <ianwanjira4@gmail.com>
2025-02-01 17:45:19 +0300

    style: Add styles for Button component
    - Created styles for the  component using React Native's StyleSheet.
    - Defined button styles including background color, padding, and border radius.
    - Added text styles for button text color and font size.
    - Included disabled button style to reduce opacity when loading.

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  button: {
+    width: '100%',
+    backgroundColor: '#007BFF',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  disabledButton: {
+    opacity: 0.5,
+  },
+});
+
+export {styles};
