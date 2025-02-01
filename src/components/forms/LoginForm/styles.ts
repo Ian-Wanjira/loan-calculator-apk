4781d69e87f7ec5868bfe2b83b5a501b3544256a
Ian-Wanjira <ianwanjira4@gmail.com>
2025-02-01 17:59:19 +0300

    Add styles for LoginForm component
    - Created a new styles.ts file to define styles for the LoginForm component.
    - Added styles for the login button and button text.
    - Ensured consistent design with padding, background color, and border radius.

---
+++ src/components/forms/LoginForm/styles.ts
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  button: {
+    width: '100%',
+    backgroundColor: '#007BFF',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
+
+export {styles};
