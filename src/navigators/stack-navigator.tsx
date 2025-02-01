d3c792a9d16120d6398a89de0adda98f121e9ccc
Ian-Wanjira <ianwanjira4@gmail.com>
2025-02-01 19:10:25 +0300

    feat: Add Register screen to AuthStack
    - Included Register screen in the authentication stack navigator.
    - Updated imports to reflect the addition of the Register component.

--- src/navigators/stack-navigator.tsx
+++ src/navigators/stack-navigator.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {Home, Login, Results} from '../screens';
+import {Home, Login, Results, Register} from '../screens';
 
 const Stack = createNativeStackNavigator();
 
 const AuthStack = () => (
   <Stack.Navigator screenOptions={{headerShown: false}}>
     <Stack.Screen name="Login" component={Login} />
+    <Stack.Screen name="Register" component={Register} />
   </Stack.Navigator>
 );
 
