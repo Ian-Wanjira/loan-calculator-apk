ec5664191155a17fcd704772f3ab2d764a931b4b
Ian-Wanjira <ianwanjira4@gmail.com>
2025-02-02 23:53:31 +0300

    feat: add transformers for loan calculator API
    - Implement transformLoanCalculatorResponse to map API response to internal format.
    - Create transformLoanCalculatorRequest to format input data for API requests.
    - Define interfaces for raw API response and transformed response types.

---
+++ src/utils/transformers.ts
@@ -0,0 +1,53 @@
+import {LoanCalculatorResponse} from '../types/api';
+import {LoanCalculatorFromInputs} from '../components/forms/LoanCalculatorForm/LoanCalculatorForm';
+
+export interface LoanCalculatorResponseRaw {
+  'Payment Every Month': number;
+  'Total Payments': number;
+  'Total Interest': number;
+  'Principal Percentage': number;
+  'Interest Percentage': number;
+  'Amortization Schedule': {
+    Period: number;
+    'Beginning Balance': number;
+    Interest: number;
+    Principal: number;
+    'Ending Balance': number;
+  }[];
+}
+
+export const transformLoanCalculatorResponse = (
+  apiResponse: LoanCalculatorResponseRaw,
+): LoanCalculatorResponse => {
+  return {
+    paymentEveryMonth: apiResponse['Payment Every Month'],
+    totalPayments: apiResponse['Total Payments'],
+    totalInterest: apiResponse['Total Interest'],
+    principalPercentage: apiResponse['Principal Percentage'],
+    interestPercentage: apiResponse['Interest Percentage'],
+    amortizationSchedule: apiResponse['Amortization Schedule'].map(entry => ({
+      period: entry.Period,
+      beginningBalance: entry['Beginning Balance'],
+      interest: entry.Interest,
+      principal: entry.Principal,
+      endingBalance: entry['Ending Balance'],
+    })),
+  };
+};
+
+export const transformLoanCalculatorRequest = ({
+  apiRequest,
+}: {
+  apiRequest: LoanCalculatorFromInputs;
+}) => {
+  return {
+    loan_amount: apiRequest.loanAmount,
+    interest_rate: apiRequest.interestRate,
+    loan_term: {
+      years: apiRequest.loanTerm.years ?? 0,
+      months: apiRequest.loanTerm.months ?? 0,
+    },
+    compound: apiRequest.compound,
+    pay_back: apiRequest.payBack,
+  };
+};
