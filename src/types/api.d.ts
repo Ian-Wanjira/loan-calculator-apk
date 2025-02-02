14dd48ecd302999eda79f69ac8e809af3599212b
Ian-Wanjira <ianwanjira4@gmail.com>
2025-02-02 23:48:31 +0300

    feat: add types for Loan Calculator API
    - Introduce CompoundOption and PaybackOption types for loan calculation options.
    - Define LoanCalculatorResponse interface for API response structure.
    - Add AmortizationScheduleEntry interface for detailed payment breakdown.
    - Create LoanCalculatorApiRequest interface for request payload structure.

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -1,4 +1,58 @@
+export type CompoundOption =
+  | 'Annually (APY)'
+  | 'Semi-annually'
+  | 'Quarterly'
+  | 'Monthly (APR)'
+  | 'Semi-monthly'
+  | 'Biweekly'
+  | 'Weekly'
+  | 'Daily'
+  | 'Continuously';
+export type PaybackOption =
+  | 'Every Day'
+  | 'Every Week'
+  | 'Every 2 Weeks'
+  | 'Every Half Month'
+  | 'Every Month'
+  | 'Every Quarter'
+  | 'Every 6 Months'
+  | 'Every Year';
+
 export interface LoginResponse {
   access: string;
   refresh: string;
+}
+
+export interface RegisterResponse {
+  id: string;
+  email: string;
+  name: string;
+}
+
+export interface LoanCalculatorResponse {
+  paymentEveryMonth: number;
+  totalPayments: number;
+  totalInterest: number;
+  principalPercentage: number;
+  interestPercentage: number;
+  amortizationSchedule: AmortizationScheduleEntry[];
+}
+
+export interface AmortizationScheduleEntry {
+  period: number;
+  beginningBalance: number;
+  interest: number;
+  principal: number;
+  endingBalance: number;
+}
+
+export interface LoanCalculatorApiRequest {
+  loan_amount: number;
+  interest_rate: number;
+  loan_term: {
+    years: number;
+    months: number;
+  };
+  compound: string;
+  pay_back: string;
 }
